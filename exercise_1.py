8917796472d7892f7989882ed48205a54187f4f5
Olga <sumopetrus@gmail.com>
2021-09-22 12:45:45 +0200

    maj fonction liste_nom_products

--- exercise_1.py
+++ exercise_1.py
@@ -1,4 +1,4 @@
-# Creer une fonction sans arguments qui affiche les noms des produits téléchargés 
+# Creer une fonction sans arguments qui affiche les noms des produits téléchargés
 # avec les paramètres par défaut depuis l'API de recherche avancé de openfoodfacts, 
 # ainsi que le nombe de produits téléchargés.
 
@@ -12,21 +12,25 @@ import json
 # r = requests.get("https://world.openfoodfacts.org/cgi/search.pl?search_terms=banania&search_simple=1&action=process&json=1&page_size=20&page_size=1")
 # print(r.json())
 
-def liste_of_products():
+def liste_nom_products():
 
-     
-    r = requests.get("http://world.openfoodfacts.org/cgi/search.pl",  params={
+    products_name_list = []
+
+    parametres = {
         "action":"process",
-        "page":"1",
-        "page_size":"20",
-        "json":True
-        } )
+        "json" : True,
+        "nutrition_grade": "A",
+        "page": 1,
+        "page_size": 20
+                
+    }
+    r = requests.get("https://world.openfoodfacts.org/cgi/search.pl", params=parametres)
 
-    
-    # data = json.loads(r.text)
-    # print(type(data))
-    
     data = r.json()
-    print(data)
 
-liste_of_products()
+    # dict_keys(['count', 'page', 'page_count', 'page_size', 'products', 'skip'])
+
+    one_product = data["products"][1]['product_name_fr']
+    print(one_product)
+
+liste_nom_products()
